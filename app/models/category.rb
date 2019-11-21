0d601414c17616ede6adcc1171d2af5f1003a064
渡辺 晃史 <panser098@gmail.com>
2019-11-21 15:44:12 +0900

    カテゴリー　子要素

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :articles
+  has_many :child_categories, class_name: 'Category', foreign_key: 'category_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_033219) do
+ActiveRecord::Schema.define(version: 2019_11_21_061254) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_033219) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "comments", force: :cascade do |t|
